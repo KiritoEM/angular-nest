1e4aa590b08d63b1cf43307c90c2aa39a29a73ef
KiritoEM <johankirito64@gmail.com>
2024-12-25 21:47:03 +0300

    feat: add login page template

---
+++ src/app/pages/pokemon-card/pokemon-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-card',
+  imports: [],
+  templateUrl: './pokemon-card.component.html',
+  styleUrl: './pokemon-card.component.scss',
+  standalone: true
+})
+export class PokemonCardComponent {
+  
+}
