4c1e85298079d08c8c3833b5f0e72bc1c4f38134
KiritoEM <johankirito64@gmail.com>
2024-12-30 20:17:52 +0300

    feat: add entity and mapped-types for DTO -> api

--- api/src/users/dto/user.dto.ts
+++ api/src/users/dto/user.dto.ts
@@ -1,6 +1,4 @@
-import { ApiProperty, OmitType } from "@nestjs/swagger";
-import { IsEmail } from "class-validator";
-import { IsNotEmpty, IsString } from "class-validator";
+import { OmitType } from "@nestjs/mapped-types";
 import { UserEntity } from "../entity/user.entity";
 
 export class CreateUserDto extends OmitType(UserEntity, ['id']) { }
