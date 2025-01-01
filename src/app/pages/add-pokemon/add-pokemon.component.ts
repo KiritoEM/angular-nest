5484520471f1b31068ce17a038a8388133192b0e
KiritoEM <johankirito64@gmail.com>
2025-01-01 23:27:56 +0300

    feat: add upload-file in add-pokemon

--- src/app/pages/add-pokemon/add-pokemon.component.ts
+++ src/app/pages/add-pokemon/add-pokemon.component.ts
@@ -1,11 +1,37 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-add-pokemon',
-  imports: [],
+  imports: [MatIcon, NgIf],
   templateUrl: './add-pokemon.component.html',
   styleUrl: './add-pokemon.component.scss'
 })
 export class AddPokemonComponent {
+  file: File | null = null;
+  previewURL: string | ArrayBuffer | null = null;
 
+  onChange(event: any) {
+    const file: File = event.target.files[0];
+
+    if (file) {
+      console.log(file);
+      this.file = file;
+
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        this.previewURL = reader.result;
+      }
+
+      reader.readAsDataURL(file);
+    }
+
+  }
+
+  resetInput() {
+    this.file = null;
+    this.previewURL = null;
+  }
 }
