613633c51dcaff2144bd4aca152ed2f2bdf27705
KiritoEM <johankirito64@gmail.com>
2025-01-01 10:39:08 +0300

    feat: add header to pokemon-list

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../components/shared/button/button.component';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [ButtonComponent, MatIcon],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
